72dc7946f6e7841bfb9f2100143c1a168f92b523
Sir Nightmare <ivantochilkin@gmail.com>
2016-09-25 18:18:25 +0300

    some fixes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # 3_bars
+
+This script shows which bar from the list is the biggest which is the least and which is closest to you.
+
+You can download the list of the bars here:
+
+http://data.mos.ru/opendata/7710881420-bary
+
+You can run the script using following command:
+
+python bars.py <path_to_json_file>
+
+Function of finding the closest bar  uses modification of haversine formula
+ witch can give very accurate distance in meters
+ 
+ You can find more information here:
+http://gis-lab.info/qa/great-circles.html
--- bars.py
+++ bars.py
@@ -1,6 +1,6 @@
 import sys
 import json
-import math  # for accurate_distance
+import math
 
 
 def load_data(filepath):
@@ -34,50 +34,32 @@ def get_smallest_bar(data):
     return [smallest_bar, min_num_of_seats]
 
 
+def distance(current_long, current_lat, bar_long, bar_lat):
+    earth_radius = 6372795
+
+    current_lat = current_lat * math.pi / 180.
+    bar_lat = bar_lat * math.pi / 180.
+    current_long = current_long * math.pi / 180.
+    bar_long = bar_long * math.pi / 180.
+
+    # косинусы и синусы широт и разницы долгот
+    cl1 = math.cos(current_lat)
+    cl2 = math.cos(bar_lat)
+    sl1 = math.sin(current_lat)
+    sl2 = math.sin(bar_lat)
+    delta = bar_long - current_long
+    cdelta = math.cos(delta)
+    sdelta = math.sin(delta)
+
+    # вычисления длины большого круга
+    numerator = math.sqrt(math.pow(cl2 * sdelta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2))
+    denominator = sl1 * sl2 + cl1 * cl2 * cdelta
+    angular_disparity = math.atan2(numerator, denominator)
+    distance = angular_disparity * earth_radius
+    return distance
+
+
 def get_closest_bar(data, longitude, latitude):
-    def distance(current_long, current_lat, bar_long, bar_lat, is_accurate=True):
-        """
-        simple_distance finds distance between two points in Cartesian coordinates
-        it is not very accurate and it cannot give information about actual distance
-        but it works properly if you want to find the closest bar in the city ;)
-
-        accurate_distance uses modification of haversine formula witch can give very accurate distance in meters
-        You can find more information here:
-        http://gis-lab.info/qa/great-circles.html
-        """
-
-        def simple_distance(current_long, current_lat, bar_long, bar_lat):
-            return ((bar_long - current_long) ** 2 + (bar_lat - current_lat) ** 2) ** 0.5
-
-        def accurate_distance(current_long, current_lat, bar_long, bar_lat):
-            earth_rad = 6372795
-
-            lat1 = current_lat * math.pi / 180.
-            lat2 = bar_lat * math.pi / 180.
-            long1 = current_long * math.pi / 180.
-            long2 = bar_long * math.pi / 180.
-
-            # косинусы и синусы широт и разницы долгот
-            cl1 = math.cos(lat1)
-            cl2 = math.cos(lat2)
-            sl1 = math.sin(lat1)
-            sl2 = math.sin(lat2)
-            delta = long2 - long1
-            cdelta = math.cos(delta)
-            sdelta = math.sin(delta)
-
-            # вычисления длины большого круга
-            y = math.sqrt(math.pow(cl2 * sdelta, 2) + math.pow(cl1 * sl2 - sl1 * cl2 * cdelta, 2))
-            x = sl1 * sl2 + cl1 * cl2 * cdelta
-            ad = math.atan2(y, x)
-            distance = ad * earth_rad
-            return distance
-
-        if is_accurate:
-            return accurate_distance(current_long, current_lat, bar_long, bar_lat)
-        else:
-            return simple_distance((current_long, current_lat, bar_long, bar_lat))
-
     bar_longitude, bar_latitude = data[0]['Cells']['geoData']['coordinates']
     min_distance = distance(longitude, latitude, bar_longitude, bar_latitude)
     closest_bar = data[0]['Cells']['Name']
@@ -87,7 +69,7 @@ def get_closest_bar(data, longitude, latitude):
         if dist_to_bar < min_distance:
             min_distance = dist_to_bar
             closest_bar = bar['Cells']['Name']
-    return closest_bar
+    return closest_bar, min_distance
 
 
 if __name__ == '__main__':
